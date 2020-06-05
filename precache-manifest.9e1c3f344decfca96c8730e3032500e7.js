65dcc77c9e3a1cd41327a83a35d41a0fd04ab732
Blanc <konata1145@naver.com>
2020-06-05 20:56:06 +0900

    Updates

---
+++ precache-manifest.9e1c3f344decfca96c8730e3032500e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d8011a37efd223c1b12b0b1afbc4dc",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "23f79f4c1343499fd879",
+    "url": "/movie_app_2020/static/css/main.a493da01.chunk.css"
+  },
+  {
+    "revision": "debec8286eb96642f41b",
+    "url": "/movie_app_2020/static/js/2.ce0b9a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.ce0b9a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23f79f4c1343499fd879",
+    "url": "/movie_app_2020/static/js/main.af710e70.chunk.js"
+  },
+  {
+    "revision": "86e4940ccaeed0f37e85",
+    "url": "/movie_app_2020/static/js/runtime-main.83f6590a.js"
+  }
+]);
